e4cac33a906445b4c1150c358fd0fa21b65c636a
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-07-29 00:43:47 -0500

    started notes

---
+++ notes.js
@@ -0,0 +1,10 @@
+/*
+vid 2
+var is function scoped. let and const are blocked scoped (anything inside curly brackets)
+let doesn't let you make duplicate variables, but var does....and that's bad mmkay.
+
+
+
+
+
+*/
